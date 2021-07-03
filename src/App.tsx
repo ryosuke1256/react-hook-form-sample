9367475540a6d7186f625ff29aca2cdf66f064fe
takutaku1256 <ryoll329555@docomo.ne.jp>
2021-07-03 13:38:36 +0900

    fix code

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ function App() {
       {errors.age && <p>Please enter number for age.</p>}
       <div>
         email
-        <input type="text" placeholder="Email" {...register("Email", {required: true, min: 8, pattern: /^\S+@\S+$/i})} />
+        <input type="text" placeholder="Email" {...register("Email", {required: true, min: -2, pattern: /^\S+@\S+$/i})} />
         {errors.Email && <p>The format of the email is incorrect.</p>}
       </div>
       <input type="submit" />
